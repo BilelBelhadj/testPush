5be952a9e50096d1a2719896a276c46a3a293871
Bilel Belhadj <belhadj19bi@gmail.com>
2023-02-15 01:20:13 -0400

    ajouter un 2emme message a afficher

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, bilel");
+
+        System.out.println("Hello, 9lewi");
     }
 }
